510bbce042a9aef994e2d1cf2301b6a012a087f2
wkxboot <1131204425@qq.com>
2019-01-19 16:20:51 +0800

    1.修改看门狗任务权限为低，保证可靠 2.修改日志输出命名和缓存大小，不判断输出是否完整 3.修改bootloader复位延时为1秒

--- beer_machine/Src/freertos.c
+++ beer_machine/Src/freertos.c
@@ -189,7 +189,7 @@ void MX_FREERTOS_Init(void) {
 
   /* Create the thread(s) */
   /* definition and creation of defaultTask */
-  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 256);
+  osThreadDef(defaultTask, StartDefaultTask, osPriorityBelowNormal, 0, 256);
   defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
 
   /* USER CODE BEGIN RTOS_THREADS */
